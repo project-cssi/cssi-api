55a273de5b3602b2a05211ad8cf69a59077ec842
Brion Mario <brion.2015283@iit.ac.lk>
2019-04-15 05:04:31 +0530

    chore(models): add method to generate genre meta from file

--- app/models/application.py
+++ app/models/application.py
@@ -1,9 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# (c) Copyright 2019 Brion Mario.
+# (c) This file is part of the CSSI REST API and is made available under MIT license.
+# (c) For more information, see https://github.com/brionmario/cssi-api/blob/master/LICENSE.txt
+# (c) Please forward any queries to the given email address. email: brion@apareciumlabs.com
+
+"""Models and Schemas for Applications
+
+This module handles contains the different db models and schemas
+for applications.
+
+Attributes:
+    GENRE_META_FILE_PATH (str): Path for the file containing genre meta
+    TYPE_META_FILE_PATH (str): Path for the file containing application type meta
+
+Authors:
+    Brion Mario
+
+"""
+
+import os
+import json
 from marshmallow import fields, validate
 from .. import db, ma
+
+GENRE_META_FILE_PATH = 'meta/genre.meta.json'
 
 
 class Application(db.Model):
     __tablename__ = 'application'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     type_id = db.Column(db.Integer, db.ForeignKey('application_type.id', ondelete='CASCADE'), nullable=False)
@@ -22,16 +49,40 @@ class Application(db.Model):
         return '<Application \'%s\'>' % self.name
 
 
-class Genre(db.model):
+class Genre(db.Model):
     __tablename__ = 'genre'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
 
+    @classmethod
+    def seed(cls):
+        if os.path.exists(GENRE_META_FILE_PATH):
+            with open(GENRE_META_FILE_PATH) as genre_meta_json:
+                data = json.load(genre_meta_json)
+                for genre in data['genre']:
+                    genre = Genre(name=genre['name'])
+                    genre.save()
+        else:
+            # TODO: Add exception
+            print("Couldn't locate meta file")
 
-class Type(db.model):
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return '<Genre \'%s\'>' % self.name
+
+
+class Type(db.Model):
     __tablename__ = 'application_type'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return '<ApplicationType \'%s\'>' % self.name
 
 
 class ApplicationSchema(ma.Schema):
