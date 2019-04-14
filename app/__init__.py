9f73e3cc68c92015a79368a532082646f2cee0fe
Brion Mario <brion.2015283@iit.ac.lk>
2019-04-14 18:28:57 +0530

    chore: implement basic connections

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,32 @@
-from flask import Blueprint
+import os
+
+from flask import Flask, Blueprint
 from flask_restful import Api
-from app.applications.views import Application
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from app.applications import Application
+from config import config
 
-api_bp = Blueprint('api', __name__)
-api = Api(api_bp)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Route
-api.add_resource(Application, '/application')
+db = SQLAlchemy()
+ma = Marshmallow()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # disabling sqlalchemy event system
+
+    config[config_name].init_app(app)
+
+    # Set up extensions
+    db.init_app(app)
+
+    api_bp = Blueprint('api/v1', __name__)
+    api = Api(api_bp)
+
+    # Route
+    api.add_resource(Application, '/application')
+
+    return app
