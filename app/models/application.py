a2f7a8ea831ce962193c9a312f093c8c2cdd7f2e
Brion Mario <brion.2015283@iit.ac.lk>
2019-04-15 18:15:19 +0530

    refactor(models): refactor application models and schemas

--- app/models/application.py
+++ app/models/application.py
@@ -28,26 +28,19 @@ from .. import db, ma
 GENRE_META_FILE_PATH = 'meta/genre.meta.json'
 APPLICATION_TYPE_META_FILE_PATH = 'meta/application_type.meta.json'
 
+
 class Application(db.Model):
     __tablename__ = 'application'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    type_id = db.Column(db.Integer, db.ForeignKey('application_type.id', ondelete='CASCADE'), nullable=False)
-    type = db.relationship('ApplicationType', backref=db.backref('application', lazy='dynamic'))
+    app_type_id = db.Column(db.Integer, db.ForeignKey('application_type.id', use_alter=True, name='fk_app_type_id'), nullable=False)
     description = db.Column(db.String(250), nullable=False)
     creation_date = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False)
-    genre_id = db.Column(db.Integer, db.ForeignKey('genre.id', ondelete='CASCADE'), nullable=False)
-    genre = db.relationship('Genre', backref=db.backref('application', lazy='dynamic'))
-
-    def __init__(self, name, type_id, description, genre_id):
-        self.name = name
-        self.type_id = type_id
-        self.description = description
-        self.genre_id = genre_id
+    genre_id = db.Column(db.Integer, db.ForeignKey('genre.id', use_alter=True, name='fk_genre_id'), nullable=False)
 
     def __repr__(self):
-        return '<Application \'%s\'>' % self.name
+        return self.name
 
 
 class Genre(db.Model):
@@ -55,69 +48,93 @@ class Genre(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    display_name = db.Column(db.String(250), nullable=False)
+    applications = db.relationship('Application', backref='genre', lazy='dynamic')
 
     @classmethod
     def seed(cls):
-        if os.path.exists(GENRE_META_FILE_PATH):
-            with open(GENRE_META_FILE_PATH) as genre_meta_json:
-                data = json.load(genre_meta_json)
-                for item in data['genre']:
-                    genre = Genre(name=item['name'])
-                    genre.save()
-                    print("Adding genre metadata: {}".format(genre))
+        if cls.is_table_empty(cls):
+            if os.path.exists(GENRE_META_FILE_PATH):
+                with open(GENRE_META_FILE_PATH) as genre_meta_json:
+                    data = json.load(genre_meta_json)
+                    for item in data['genre']:
+                        genre = Genre(name=item['name'], display_name=item['display_name'])
+                        genre.save()
+                        print("Adding genre metadata: {}".format(genre))
+            else:
+                # TODO: Add exception
+                print("Couldn't locate meta file")
         else:
-            # TODO: Add exception
-            print("Couldn't locate meta file")
+            print('Table is already filled')
 
     def save(self):
         db.session.add(self)
         db.session.commit()
 
+    def is_table_empty(self):
+        if not self.query.all():
+            return True
+        return False
+
     def __repr__(self):
-        return '<Genre \'%s\'>' % self.name
+        return self.display_name
 
 
 class ApplicationType(db.Model):
     __tablename__ = 'application_type'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    display_name = db.Column(db.String(100), nullable=False)
+    display_name_full = db.Column(db.String(250), nullable=False)
+    applications = db.relationship('Application', backref='app_type', lazy='dynamic')
 
     @classmethod
     def seed(cls):
-        if os.path.exists(APPLICATION_TYPE_META_FILE_PATH):
-            with open(APPLICATION_TYPE_META_FILE_PATH) as app_type_meta_json:
-                data = json.load(app_type_meta_json)
-                for item in data['types']:
-                    app_type = ApplicationType(name=item['name'])
-                    app_type.save()
-                    print("Adding application type metadata: {}".format(app_type))
+        if cls.is_table_empty(cls):
+            if os.path.exists(APPLICATION_TYPE_META_FILE_PATH):
+                with open(APPLICATION_TYPE_META_FILE_PATH) as app_type_meta_json:
+                    data = json.load(app_type_meta_json)
+                    for item in data['types']:
+                        app_type = ApplicationType(name=item['name'], display_name=item['display_name'], display_name_full=item['display_name_full'])
+                        app_type.save()
+                        print("Adding application type metadata: {}".format(app_type))
+            else:
+                # TODO: Add exception
+                print("Couldn't locate meta file")
         else:
-            # TODO: Add exception
-            print("Couldn't locate meta file")
+            print('Table is already filled')
 
     def save(self):
         db.session.add(self)
         db.session.commit()
 
+    def is_table_empty(self):
+        if not self.query.all():
+            return True
+        return False
+
     def __repr__(self):
-        return '<ApplicationType \'%s\'>' % self.name
+        return self.display_name_full
 
 
 class ApplicationSchema(ma.Schema):
     id = fields.Integer(dump_only=True)
     name = fields.String(required=True, validate=validate.Length(1, 100))
-    type_id = fields.Integer(required=True)
+    app_type = fields.String(required=True)
     description = fields.String(required=True, validate=validate.Length(1, 250))
     creation_date = fields.DateTime()
-    genre_id = fields.Integer(required=True)
+    genre = fields.String(required=True)
 
 
 class GenreSchema(ma.Schema):
     id = fields.Integer()
     name = fields.String(required=True)
+    display_name = fields.String(required=True)
 
 
-class TypeSchema(ma.Schema):
+class ApplicationTypeSchema(ma.Schema):
     id = fields.Integer()
     name = fields.String(required=True)
+    display_name = fields.String(required=True)
+    display_name_full = fields.String(required=True)
