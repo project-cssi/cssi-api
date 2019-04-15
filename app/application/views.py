54d5d2a48937e30ca8e09e58047053af84ae71a2
Brion Mario <brion.2015283@iit.ac.lk>
2019-04-15 07:03:34 +0530

    chore(views): implement application GET and POST methods

--- app/application/views.py
+++ app/application/views.py
@@ -1,13 +1,33 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from app.models import Application, ApplicationType, ApplicationSchema
+from marshmallow import Schema, fields, ValidationError, pre_load
+from .. import db
 
 application = Blueprint('application', __name__)
+
+applications_schema = ApplicationSchema(many=True)
+application_schema = ApplicationSchema()
 
 
 @application.route('/', methods=['GET'])
 def get_applications():
-    return jsonify({"message": "Get Application New API"})
+    applications = Application.query.all()
+    applications = applications_schema.dump(applications).data
+    return jsonify({"status": "success", "data": applications}), 200
 
 
 @application.route('/', methods=['POST'])
 def create_application():
-    return jsonify({"message": "Created Application New API"})
+    name = request.json['name']
+    type_id = request.json['type_id']
+    description = request.json['description']
+    genre_id = request.json['genre_id']
+
+    new_application = Application(name=name, type_id=type_id, description=description, genre_id=genre_id)
+
+    db.session.add(new_application)
+    db.session.commit()
+
+    result = application_schema.dump(new_application).data
+
+    return jsonify({"message": "Created new application {}.".format(name), "data": result})
