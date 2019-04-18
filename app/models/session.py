a4ca3cc11be6875501f932d16ef8ab259d073dc6
Brion Mario <brion.2015283@iit.ac.lk>
2019-04-19 02:21:47 +0530

    chore(models): update session model schema

--- app/models/session.py
+++ app/models/session.py
@@ -10,7 +10,7 @@ class Session(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     app_id = db.Column(db.Integer, db.ForeignKey('application.id', use_alter=True, name='fk_app_id'), nullable=False)
     creation_date = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False)
-    expected_emotions = db.Column(db.PickleType, nullable=False)
+    expected_emotions = db.Column(db.JSON, nullable=False)
     questionnaire_id = db.Column(db.Integer, db.ForeignKey('questionnaire.id', use_alter=True, name='fk_questionnaire_id'), nullable=False)
     cssi_score = db.Column(db.Float, nullable=False, default=0)
     latency_scores = db.Column(db.JSON, nullable=False, default={})
@@ -31,7 +31,15 @@ class Session(db.Model):
 
 class SessionSchema(ma.Schema):
     id = fields.Integer(dump_only=True)
-    creation_date = fields.DateTime()
-    expected_emotions = fields.List(fields.String(), dump_only=True)
+    creation_date = fields.DateTime(dump_only=True)
+    expected_emotions = fields.List(fields.String(), required=True)
     app = fields.Nested(ApplicationSchema, dump_only=True)
     questionnaire = fields.Nested(QuestionnaireSchema, dump_only=True)
+    cssi_score = fields.Float()
+    latency_scores = fields.Dict()
+    total_latency_score = fields.Float()
+    sentiment_scores = fields.Dict()
+    total_sentiment_score = fields.Float()
+    questionnaire_scores = fields.Dict()
+    total_questionnaire_score = fields.Float()
+
