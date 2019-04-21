deb693ddefe6410f50ebe3feac131fdb7cc26b67
Brion Mario <brion.2015283@iit.ac.lk>
2019-04-21 13:10:55 +0530

    chore(app): add socket io and celery init logic to app module

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 import os
 import logging.config
 
+from celery import Celery
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_socketio import SocketIO
+
 from config import CONFIG
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -17,30 +20,51 @@ try:
 except OSError:
     pass
 
-# load logging config file
+# Load logging config file
 logging.config.fileConfig('config/logging.conf', disable_existing_loggers=False)
-# init file logger
+# Init file logger
 logger = logging.getLogger('CSSI_REST_API')
 
 db = SQLAlchemy()
 ma = Marshmallow()
+socketio = SocketIO()
+celery = Celery(__name__,
+                broker=os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379'),
+                backend=os.environ.get('CELERY_BACKEND', 'redis://localhost:6379'))
+celery.config_from_object('celeryconfig')
+
+# Import models to register them with SQLAlchemy
+from app.models import Application, Genre, ApplicationType, Session, Questionnaire
 
 
-def create_app(config_name):
+def create_app(config_name=None, main=True):
     app = Flask(__name__)
-    CORS(app, support_credentials=True)
     app.config.from_object(CONFIG[config_name])
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # disabling sqlalchemy event system
+
+    CORS(app, support_credentials=True)  # Add CORS support
 
     CONFIG[config_name].init_app(app)
 
     root = CONFIG[config_name].APPLICATION_ROOT
 
-    # flask migrate doesn't recognize the tables without this import
-    from app.models import Application, Genre, ApplicationType, Session, Questionnaire
+
 
     # Set up extensions
     db.init_app(app)
+
+    if main:
+        # Initialize socketio server and attach it to the message queue.
+        socketio.init_app(app,
+                          message_queue=app.config['SOCKETIO_MESSAGE_QUEUE'])
+    else:
+        # Initialize socketio to emit events through through the message queue.
+        socketio.init_app(None,
+                          message_queue=app.config['SOCKETIO_MESSAGE_QUEUE'],
+                          async_mode='threading')
+
+    celery.conf.update(CONFIG[config_name].CELERY_CONFIG)
 
     # Create app blueprints
     from app.routes.v1 import main as main_blueprint
